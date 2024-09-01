00f621fdc97e239b72a8630f44c30ba03265d152
SadiqAmaan <amaankhan1666@gmail.com>
2024-09-01 22:26:20 +0530

    Changes

--- src/Components/Global/Navbar/Banner/Banner.jsx
+++ src/Components/Global/Navbar/Banner/Banner.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { BannerWrapper } from './BannerStyle';
 
 const Banner = () => {
-    return (
-        <main className="p-8">
-          <section className="relative bg-black rounded-lg overflow-hidden">
-            <img src="/images/Banner.jpg" alt="Living Room" className="w-full h-auto object-cover opacity-70" />
-            <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
-              <h2 className="text-4xl font-bold text-white">Z&Z Interiors</h2>
-            </div>
-          </section>
-        </main>
-      );
+  return (
+    <BannerWrapper>
+      <section className='banner'>
+        <img src="/images/Banner.jpg" alt="Living Room" />
+        <div className='title'>
+          <h2>Z&Z Interiors</h2>
+        </div>
+      </section>
+    </BannerWrapper>
+  );
 }
 
-export default Banner
+export default Banner;
---
+++ src/Components/Global/Navbar/Banner/BannerStyle.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+export const BannerWrapper = styled.main`
+  padding: 2rem;
+
+  .banner {
+    position: relative;
+    background-color: black;
+    border-radius: 1rem;
+    overflow: hidden;
+    margin: 1rem;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+
+    img {
+      width: 100%;
+      height: 600px;
+      object-fit: cover;
+      opacity: 0.7;
+
+      @media (max-width: 768px) {
+        height: 400px;
+      }
+
+      @media (max-width: 480px) {
+        height: 300px;
+      }
+    }
+
+    div {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      h2 {
+        font-size: 4rem;
+        font-weight: bold;
+        color: white;
+
+        @media (max-width: 768px) {
+          font-size: 3rem;
+        }
+
+        @media (max-width: 480px) {
+          font-size: 2rem;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    padding: 1.5rem;
+  }
+
+  @media (max-width: 480px) {
+    padding: 1rem;
+  }
+`;
